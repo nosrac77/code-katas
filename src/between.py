1e72eb436353bd312b84f55efa0032905f231d2f
nosrac77 <carson.newton@outlook.com>
2017-10-21 23:28:17 -0700

    another kata completed, all tests pass.

---
+++ src/between.py
@@ -0,0 +1,13 @@
+"""Kata: Fibonacci's FizzBuzz - return a list of the fibonacci sequence with
+the words 'Fizz', 'Buzz', and 'FizzBuzz' replacing certain integers.
+
+#1 Best Practices Solution by zebulan, lancelote, Paulchenkiller,
+antoine.leclercq, VadimPopov, Jonathan.Chan (plus 93 more warriors)
+
+def between(a,b):
+    return range(a,b+1)
+"""
+
+
+def between(a, b):
+    return ([x for x in range(a, b+1)])
